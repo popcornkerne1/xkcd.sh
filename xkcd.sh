c46f17c4054aa51a1ec9f2826009be1c85bebd64
posixcompliant <92425935+popcornkerne1@users.noreply.github.com>
2026-01-16 12:54:26 +1100

    Final commit probably unless i decide to procrastinate more

--- xkcd.sh
+++ xkcd.sh
@@ -7,9 +7,9 @@ if [ ! -d "$XKCDIR" ]; then
 fi
 
 if [ -z "$1" ]; then
-	curl -s https://xkcd.com/info.0.json > $XKCDIR/current_xkcd.json
+	curl -s https://xkcd.com/info.0.json >$XKCDIR/current_xkcd.json
 	echo $(jq -r '.title' $XKCDIR/current_xkcd.json) && curl -s $(jq -r '.img' $XKCDIR/current_xkcd.json) | kitten icat --transfer-mode=stream && echo $(jq -r '.alt' $XKCDIR/current_xkcd.json)
-  else 
-	curl -s https://xkcd.com/$1/info.0.json > $XKCDIR/$1.json
+else
+	curl -s https://xkcd.com/$1/info.0.json >$XKCDIR/$1.json
 	echo $(jq -r '.title' $XKCDIR/$1.json) && curl -s $(jq -r '.img' $XKCDIR/$1.json) | kitten icat --transfer-mode=stream && echo $(jq -r '.alt' $XKCDIR/$1.json)
 fi
